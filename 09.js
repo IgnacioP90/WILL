e5e5616310ecb1706878181fb2d6f1f6025728c2
Ignacio <nacho_handball_90@hotmail.com>
2023-01-07 17:40:41 -0300

    first commit

--- 09.js
+++ 09.js
@@ -24,7 +24,15 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
-
+  Array.prototype.filtrar = function (funcion){
+    let result = [];
+    for(var i = 0; i < this.length-1; i++){
+      if(funcion(this[i])){
+        result.push(this[i])
+      }
+      }
+      return result;
+  }
 };
 
 // No modifiques nada debajo de esta linea //
